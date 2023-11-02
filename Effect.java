7cc7f19c26cc3c6442b4becfdfa3f7691fa543bc
Gergana Mahlelieva <116373050+gerganamahleliewa@users.noreply.github.com>
2023-11-02 13:07:34 +0200

    Create Effect.java

---
+++ Effect.java
@@ -0,0 +1,18 @@
+public class Effect {
+
+    private String descriptions;
+
+   public Effect(String descriptions)
+   {
+       this.descriptions = descriptions;
+   }
+
+   public void setDescriptions(String descriptions)
+   {
+       this.descriptions = descriptions;
+   }
+   public String getDescriptions()
+   {
+       return descriptions;
+   }
+}
